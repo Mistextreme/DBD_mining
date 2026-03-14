c1c1ba87398ad273851695d5439647a028071d33
Mist the Goat <62227356+Mistextreme@users.noreply.github.com>
2026-03-14 14:45:11 +0000

    Upload: mining.meta.lua

---
+++ config/mining.meta.lua
@@ -0,0 +1,55 @@
+Config = Config or {}
+Config.Mining = Config.Mining or {}
+Config.Mining.Meta = {
+    -- Task NPC (used when Config.Mining.Tasks.Enabled)
+    TaskNpc = {
+        model = 'a_m_m_hillbilly_01',
+        coords = vec4(2944.83, 2746.82, 43.38, 284.63),
+    },
+    [1] = {
+        Center = vec3(2946.6995, 2792.2271, 40.5708),
+        Hours = { Enabled = true, Open = 0, Close = 24 },
+    },
+    [2] = {
+        Center = vec3(2946.6995, 2792.2271, 40.5708),
+        Hours = { Enabled = true, Open = 0, Close = 24 },
+    },
+    [3] = {
+        Center = vec3(2946.6995, 2792.2271, 40.5708),
+        Hours = { Enabled = true, Open = 0, Close = 24 },
+    },
+    [4] = {
+        Center = vec3(2946.6995, 2792.2271, 40.5708),
+        Hours = { Enabled = true, Open = 0, Close = 24 },
+    },
+    [5] = {
+        Center = vec3(2946.6995, 2792.2271, 40.5708),
+        Hours = { Enabled = true, Open = 0, Close = 24 },
+    },
+    [6] = {
+        Center = vec3(2946.6995, 2792.2271, 40.5708),
+        Hours = { Enabled = true, Open = 0, Close = 24 },
+    },
+    [7] = {
+        Center = vec3(2946.6995, 2792.2271, 40.5708),
+        Hours = { Enabled = true, Open = 0, Close = 24 },
+    },
+    [8] = {
+        Center = vec3(2946.6995, 2792.2271, 40.5708),
+        Hours = { Enabled = true, Open = 0, Close = 24 },
+    },
+    [9] = {
+        Center = vec3(2156.93, 6106.98, 57.73),
+        Hours = { Enabled = true, Open = 0, Close = 24 },
+    },
+    [10] = {
+        Center = vec3(-1015.13, 4403.29, 14.5),
+        Hours = { Enabled = true, Open = 0, Close = 24 },
+    },
+    [11] = {
+        Center = vec3(4290.3, -4485.46, 3.32),
+        Hours = { Enabled = true, Open = 0, Close = 24 },
+    },
+}
+
+return Config
